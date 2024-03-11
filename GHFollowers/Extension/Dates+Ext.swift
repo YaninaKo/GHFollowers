fd6ae3153851d90a9ce4f8652a93ba99ebbe2e63
YaninaKo <yanina.kovrakh@gmail.com>
2024-03-11 21:53:09 +0100

    Initial commit

---
+++ GHFollowers/Extension/Dates+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Dates+Ext.swift
+//  GHFollowers
+//
+//  Created by Yanina Kovrakh on 04.03.2024.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMontYearFormat() -> String {
+        let dateFormatter           = DateFormatter()
+        dateFormatter.dateFormat    = "MMM yyyy"
+        
+        return dateFormatter.string(from: self)
+    }
+}
